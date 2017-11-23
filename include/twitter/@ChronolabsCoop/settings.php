d34a49efec91b605659788a0b07ab994062c003b
Dr. Antony Roberts <simon@snails.email>
2017-11-24 00:42:20 +1100

    Twitter Time/Calendar Annoucing Cronjob!

--- include/twitter/@ChronolabsCoop/settings.php
+++ include/twitter/@ChronolabsCoop/settings.php
@@ -1,9 +1,9 @@
 <?php
 
-    return array(   'twitter'      =>  array(   'consumer_key'                  =>  'fk97aQVuoSY5syo9rNQiF1a6o',
-                                                'consumer_secret'               =>  'Sj8its8v8yQnEZtMrE3hm9BqgfhkyF6x2nG9081BPoRhgtVNsk',
-                                                'oauth_access_token'            =>  '2916941286-w2hKqu2obzyVM9VkF2FmUedEgmKdEWtosbfb6IM',
-                                                'oauth_access_token_secret'     =>  '6CRttMfnudkWhparEtWI5CEF9TOR13KKPICLDKix0I3Fl',
+    return array(   'twitter'      =>  array(   'consumer_key'                  =>  'w2hKqu2obzyVM9VkF2FmUedEgmKdEWtosbfb6IM',
+                                                'consumer_secret'               =>  'w2hKqu2obzyVM9VkF2FmUedEgmKdEWtosbfb6IM',
+                                                'oauth_access_token'            =>  'w2hKqu2obzyVM9VkF2FmUedEgmKdEWtosbfb6IM',
+                                                'oauth_access_token_secret'     =>  'w2hKqu2obzyVM9VkF2FmUedEgmKdEWtosbfb6IM',
                                                 'username'                      =>  'ChronolabsCoop'
                                    ),
                     'timezone'     =>   'Australia/Sydney'
